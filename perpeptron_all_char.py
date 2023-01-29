f85708ab1954ccf15fcd85c736fb42b91845bbab
cipry <34208405+cipry18@users.noreply.github.com>
2023-01-29 16:00:48 +0200

    modified perceptron_all_char.py

--- perpeptron_all_char.py
+++ perpeptron_all_char.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pandas as pd
 import string
-import pickle
 from matplotlib import pyplot as plt
 
-import another_test as p
+import main as p
 
 pd.options.mode.chained_assignment = None  # default='warn'
 data_train = pd.read_csv('letters/emnist-letters-train.csv', header=None)
@@ -24,13 +23,10 @@ for i, l in enumerate(string.ascii_uppercase, start=1):
     letter_2.iloc[:, 0] = 0
 
     letter_all = pd.concat([letter, letter_2]).sample(frac=1)
-    # letter_all.to_csv(f"all_letter_{l}.csv")
     x_train = letter_all.drop(letter_all.columns[0], axis=1).to_numpy()
     y_train = letter_all.iloc[:, 0].to_numpy()
     x_train = x_train / 255
-    # plt.imshow(np.resize(x_train[1], (28, 28)))
-    # plt.axis('off')
-    # plt.show()
+
     P = p.Perceptron(letter=l, epochs=150, lr=0.01)
     P.train(x_train, y_train)
     plt.imshow(np.resize(P.weights, (28, 28)))
@@ -44,8 +40,3 @@ for i in range(0, 5):
         prediction = perc.predict(letter_T[i])
         print(perc.letter, ' - ', prediction)
 
-
-# prediction = perceptron_list[20].predict(letter_T[4])
-# print(prediction)
-# prediction = perceptron_list[19].predict(letter_T[4])
-# print(prediction)
